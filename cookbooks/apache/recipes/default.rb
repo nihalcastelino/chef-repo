dcb5332eec27ea0e457f993cacb86ce4ca1f16e5
nihalcastelino <nihalcastelino@gmail.com>
2018-05-11 22:50:22 +0100

    Basic Apache Cookbook updated with motd etc

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,12 +3,19 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-package 'apache2' do
-    package_name 'httpd'
+
+if node['platform_family'] == "rhel"
+    package = "httpd"
+elsif node['platform_family'] == "debian"
+    package = "apache2"
+end
+
+package 'httpd' do
+    package_name package
     action :install
 end
 
-service 'apache2' do
+service 'httpd' do
     service_name 'httpd'
     action [:start, :enable]
 end
